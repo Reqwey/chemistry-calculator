aed852061d6f38a6f2a6033f6d4dbea786fa8b61
Reqwey <50829219+Reqwey@users.noreply.github.com>
2023-01-10 16:47:30 +0800

    :art: Make it `React`

---
+++ src/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState, Component } from "react";
+import * as ReactDOM from "react-dom/client";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Button from "@mui/joy/Button";
+import Sheet from "@mui/joy/Sheet";
+import Typography from '@mui/joy/Typography';
+import Input from '@mui/joy/Input';
+import Link from '@mui/joy/Link';
+import ChemistryCalc from './calc'
+
+export default function ReqApp() {
+  const [inputedValue, updateInputedValue] = useState('');
+  const [formula, updateFormula] = useState('CaCO3');
+
+  function handleChange(e) {
+    updateInputedValue(e.target.value);
+  }
+
+  function handlePost() {
+    updateFormula(inputedValue);
+  }
+
+  return (
+    <CssVarsProvider>
+      <main>
+        <Sheet
+          sx={{
+            width: 600,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & botom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+          }}
+          variant="outlined"
+        >
+          <div>
+            <Typography level="h2" component="h1" align="center">
+              <b>分子量计算器</b>
+            </Typography>
+            <Typography level="body2" align="center">输入化学方程式，我们帮你计算它的分子量.</Typography>
+          </div>
+          <Input
+            // html input attribute
+            placeholder="CaCO3"
+            // pass down to FormLabel as children
+            onChange={
+              handleChange.bind(this)
+            }
+            color="info"
+          />
+          <Button
+            sx={{ mt: 1 /* margin top */ }}
+            onClick={handlePost}
+            color="info"
+          >开始计算</Button>
+          <ChemistryCalc formula={formula} />
+        </Sheet>
+      </main>
+    </CssVarsProvider>
+  );
+}
+
+ReactDOM.createRoot(document.querySelector("#app")).render(
+  <React.StrictMode>
+    <ReqApp />
+  </React.StrictMode>
+);

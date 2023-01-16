228daf0ec683ecf6e19d893847f4f8aad6a2e2ce
Reqwey <50829219+Reqwey@users.noreply.github.com>
2023-01-16 17:31:02 +0800

    use `memo` to improve performance

--- src/components/modetoggle.jsx
+++ src/components/modetoggle.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useColorScheme } from '@mui/joy/styles';
-import { Box, Button } from '@mui/joy';
+import { Button } from '@mui/joy';
 import LightMode from '@mui/icons-material/LightModeTwoTone';
 import DarkMode from '@mui/icons-material/DarkModeTwoTone';
 
@@ -18,23 +18,14 @@ export default function ModeToggle() {
 	}
 
 	return (
-		<Box
-			sx={{
-				width: 600,
-				mx: 'auto', // margin left & right
-				my: 4, // margin top & botom
-				gap: 2
+		<Button
+			variant="soft"
+			startDecorator={mode === 'light' ? <LightMode /> : <DarkMode />}
+			onClick={() => {
+				setMode(mode === 'light' ? 'dark' : 'light');
 			}}
 		>
-			<Button
-				variant="soft"
-				startDecorator={mode === 'light' ? <LightMode /> : <DarkMode />}
-				onClick={() => {
-					setMode(mode === 'light' ? 'dark' : 'light');
-				}}
-			>
-				{mode === 'light' ? '关灯' : '开灯'}
-			</Button>
-		</Box>
+			{mode === 'light' ? '关灯' : '开灯'}
+		</Button>
 	);
 }
